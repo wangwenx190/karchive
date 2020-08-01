7d86dc9c686a177090b5bc132f555d3274099166
Yuhang Zhao <2546789017@qq.com>
2020-08-01 10:00:27 +0800

    Update.
    Signed-off-by: Yuhang Zhao <2546789017@qq.com>

--- karchive/karchive_global.h
+++ karchive/karchive_global.h
@@ -3,9 +3,13 @@
 #include <QtCore/qglobal.h>
 
 #ifndef KARCHIVE_EXPORT
+#ifdef KARCHIVE_STATIC
+#define KARCHIVE_EXPORT
+#else
 #ifdef KARCHIVE_LIBRARY
 #define KARCHIVE_EXPORT Q_DECL_EXPORT
 #else
 #define KARCHIVE_EXPORT Q_DECL_IMPORT
+#endif
 #endif
 #endif
